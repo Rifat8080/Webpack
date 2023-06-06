9c2c9fc0137a6441246b7b8dad716c55799437e6
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-06-06 14:32:54 +0600

    webpack has been setup properly

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   mode:'development',
+   entry: {
+     index: './src/index.js',
+     print: './src/print.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Output Management',
+       template:'src/template.html'
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+      module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+ };
+ 
